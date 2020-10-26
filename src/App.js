9435b9fcaacdfa54ceef8961f1cc89a3e097c66d
denisjovic <denis.jovic@tn-tech.co.rs>
2020-10-26 11:09:14 +0100

    Revert "first commit"
    This reverts commit 54e80c6fb244f92968d0eac1bd9a3561374ee13d.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import React, { Component } from 'react';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-class App extends Component {
-
-  state = {
-    notes : [
-      {
-        id: 1,
-        note: "Hello world"
-      }
-    ]
-  }
-  render() {
-    // Destructuring state
-    const {notes} = this.state
-
+function App() {
   return (
-    <div>
-    <AmplifySignOut />
-    
-    {/* Add note */}
-    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
-      <h1 className="code f2-1">Amplify Notetaker</h1>
-      <form action="" className="mb3">
-        <input 
-        type="text" 
-        className="pa2 f4"
-        placeholder="Write your note..."
-        />
-        <button 
-        className="pa2 f4" 
-        type="submit">Add note</button>
-      </form>
-
-      {/* Notes list */}
-      <div>
-        {notes.map(item => (
-          <div key={item.id} className="flex items-center">
-            <li className="list pa1 f3">
-                {item.note}
-            </li>
-            <button className="bg-transparent bn f4"><span>&times;</span></button>
-          </div>
-        ))}
-      </div>
-
-    </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-  }
 }
-// includeGreetings seems not be working anymore
-export default withAuthenticator(App, { includeGreetings: true });
+
+export default App;
